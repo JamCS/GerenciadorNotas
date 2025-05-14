f75507d3489cde0c632d5bf74da1ed7cca1e097a
JamCS <jamilsoncnn777@gmail.com>
2025-05-14 03:58:48 -0300

    desistencia do background, criação registroAluno, alteração no model.Aluno

--- boletimUP/src/boletimup/model/Aluno.java
+++ boletimUP/src/boletimup/model/Aluno.java
@@ -2,11 +2,14 @@ package boletimup.model;
 
 public class Aluno {
     private String nome;
-    private String ra;
+    private String matricula;
+    private double notaA1;
+    private double notaA2;
+    private double notaA3;
     
-    public Aluno(String nome, String ra){
+    public Aluno(String nome, String matricula){
         this.nome = nome;
-        this.ra = ra;
+        this.matricula = matricula;
     }
     
     //métodos get/set para nome do aluno
@@ -20,10 +23,34 @@ public class Aluno {
     
     //métodos get/set para ra do aluno
     public String getra(){
-        return ra;
+        return matricula;
     }
     
-    public void setra(String ra){
-        this.ra = ra;
+    public void setra(String rmatricula){
+        this.matricula = rmatricula;
+    }
+
+    public double getNotaA1() {
+        return notaA1;
+    }
+    
+    public void setNotaA1(double notaA1) {
+        this.notaA1 = notaA1;
+    }
+
+    public double getNotaA2() {
+        return notaA2;
+    }
+    
+    public void setNotaA2(double notaA2) {
+        this.notaA2 = notaA2;
+    }
+
+    public double getNotaA3() {
+        return notaA3;
+    }
+    
+    public void setNotaA3(double notaA3) {
+        this.notaA3 = notaA3;
     }
 }
--- boletimUP/src/boletimup/view/RegistroAlunoView.java
+++ boletimUP/src/boletimup/view/RegistroAlunoView.java
@@ -8,12 +8,12 @@ package boletimup.view;
  *
  * @author JamilsonCS
  */
-public class RegistroDeAlunos extends javax.swing.JFrame {
+public class RegistroAlunoView extends javax.swing.JFrame {
 
     /**
-     * Creates new form RegistroDeAlunos
+     * Creates new form RegistroAlunoView
      */
-    public RegistroDeAlunos() {
+    public RegistroAlunoView() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class RegistroDeAlunos extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(RegistroDeAlunos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistroAlunoView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(RegistroDeAlunos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistroAlunoView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(RegistroDeAlunos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistroAlunoView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(RegistroDeAlunos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistroAlunoView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new RegistroDeAlunos().setVisible(true);
+                new RegistroAlunoView().setVisible(true);
             }
         });
     }
